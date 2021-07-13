ad957b9a03ff238e5114f2a43305a1ebb7b9bc1a
Nirmesh Rastogi <you@example.com>
2021-07-13 20:36:18 +0530

    understand context

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { UserProvider } from './UserContext';
+import Name from './NameComponent';
+import Location from './LocationComponent';
+import UserForm from './UserForm';
+import ScotchInfoBar from './ScotchInfoBar';
+import './styles.css';
+
+// This component updates with data from context
+function App() {
+  return (
+    <div className="App">
+      <h2 className="is-size-4">
+        We'll use the React Context API to pass and receive data in
+        any component.
+      </h2>
+      <div className="container">
+        <UserForm />
+
+        <h2 className="is-size-4">Display User Info Here 👇</h2>
+        <p>
+          These two children components will receive data. These could
+          be nested components.
+        </p>
+        <Name />
+        <Location />
+      </div>
+      <ScotchInfoBar />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <UserProvider>
+    <App />
+  </UserProvider>,
+  rootElement
+);
